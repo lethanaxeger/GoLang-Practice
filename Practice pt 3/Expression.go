8154b619eab3f6539923650c1f780abdba150370
LethanAxeger <53499521+lethanaxeger@users.noreply.github.com>
2022-07-16 21:29:28 +0700

    Added Switch Expression
    Added switch expression

--- Practice pt 3/Expression.go
+++ Practice pt 3/Expression.go
@@ -7,7 +7,6 @@ func main() {
 	var input = "Hakim"
 
 	// Simple if Expression
-
 	if input == "Bashir" {
 		fmt.Println("Halo " + input)
 	} else {
@@ -30,5 +29,45 @@ func main() {
 		fmt.Println("Nama terlalu panjang")
 	} else {
 		println("Nama sudah benar")
+	}
+
+	// Switch Expression
+	name := "Fadlillah"
+
+	switch name {
+	case "Fadlillah":
+		fmt.Println("Hello Fadlillah")
+
+	case "Bashir":
+		fmt.Println("Hello Bashir")
+
+	case "Hakim":
+		fmt.Println("Hello Hakim")
+
+	default:
+		fmt.Println("Siapakah gerangan?")
+	}
+
+	// Short Statement
+	switch length := len(name); length > 5 {
+	case true:
+		fmt.Println("Nama terlalu panjang")
+
+	case false:
+		fmt.Println("Nama sudah benar")
+
+	}
+
+	// Switch without any condition
+	length := len(name)
+	switch {
+	case length > 10:
+		fmt.Println("Nama terlalu panjang")
+
+	case length > 5:
+		fmt.Println("Nama lumayan panjang")
+
+	default:
+		fmt.Println("Nama sudah benar")
 	}
 }
