5c4ff74db9ba5bfdf535c3c5c86eea63f0fb011c
LethanAxeger <fadlillah_bashir_28rpl@student.smktelkom-mlg.sch.id>
2022-07-18 13:48:22 +0700

    -

--- Practice pt 4/struct.go
+++ Practice pt 4/struct.go
@@ -1,6 +1,27 @@
 package main
 
+import "fmt"
+
 type Customer struct {
 	Name, Address string
-	Age int
+	Age           int
+}
+
+func main() {
+	var eko Customer
+	eko.Name = "Eko"
+	eko.Address = "Malang"
+	eko.Age = 18
+
+	fmt.Println(eko.Name)
+	fmt.Println(eko.Address)
+	fmt.Println(eko.Age)
+
+	bashir := Customer{
+		Name: "Bashir",
+		Age: 19,
+		Address: "Sawojajar",
+	}
+	fmt.Println(bashir)
+	
 }
